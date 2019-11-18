cbae278c64218d7786b1a94f9da5170b5514baec
giovdigua <giovdigua@gmail.com>
2019-11-18 10:33:13 +0100

    fixed bugs, code optimization

--- main.js
+++ main.js
@@ -2,42 +2,39 @@
 var arrayMail = ['bruceSpringsteen@rockstar.com','eltonJohn@rockstar.com','rogerWaters@rockstar.com','paulMcCartney@rockstar.com','mickJagger@rockstar.com'];
 
 var mailToCheck = prompt("Insert your mail address:");
-
+var mailFound;
 for (var i = 0; i < arrayMail.length; i++) {
     if (mailToCheck == arrayMail[i]){
-        if (arrayMail[i] == 'bruceSpringsteen@rockstar.com') {
-            document.getElementById('rockstarContainer').setAttribute('class','show');
-            document.getElementById('rockstar').innerHTML = '"BOSS"';
-            document.getElementById("avatar").src = "images/bruce.jpeg";
-        }
-        if (arrayMail[i] == 'eltonJohn@rockstar.com') {
-            document.getElementById('rockstarContainer').setAttribute('class','show');
-            document.getElementById('rockstar').innerHTML = '"Elton"';
-            document.getElementById("avatar").src = "images/elton.jpeg";
-
-        }
-        if (arrayMail[i] == 'rogerWaters@rockstar.com') {
-                document.getElementById('rockstarContainer').setAttribute('class','show');
-                document.getElementById('rockstar').innerHTML = '"Roger"';
-                document.getElementById("avatar").src = "images/roger.jpeg";
-
-        }
-        if (arrayMail[i] == 'paulMcCartney@rockstar.com') {
-            document.getElementById('rockstarContainer').setAttribute('class','show');
-            document.getElementById('rockstar').innerHTML = '"Paul"';
-            document.getElementById("avatar").src = "images/paul.jpeg";
-
-        }
-        if (arrayMail[i] == 'mickJagger@rockstar.com') {
-            document.getElementById('rockstarContainer').setAttribute('class','show');
-            document.getElementById('rockstar').innerHTML = '"Mick"';
-            document.getElementById("avatar").src = "images/mick.jpeg";
-
-        }
-    }else {
-        document.getElementById('rockstarContainer').setAttribute('class','show');
-        document.getElementById('rockstar').innerHTML = 'unknown rockstar';
-        document.getElementById("avatar").src = "images/rockstar.png";
+        mailFound = mailToCheck;
     }
+}
 
+if (mailFound == 'bruceSpringsteen@rockstar.com') {
+    document.getElementById('rockstarContainer').setAttribute('class','show');
+    document.getElementById('rockstar').innerHTML = '"BOSS"';
+    document.getElementById("avatar").src = "images/bruce.jpeg";
+}else if (arrayMail[i] == 'eltonJohn@rockstar.com') {
+    document.getElementById('rockstarContainer').setAttribute('class','show');
+    document.getElementById('rockstar').innerHTML = '"Elton"';
+    document.getElementById("avatar").src = "images/elton.jpeg";
+
+} else if (arrayMail[i] == 'rogerWaters@rockstar.com') {
+        document.getElementById('rockstarContainer').setAttribute('class','show');
+        document.getElementById('rockstar').innerHTML = '"Roger"';
+        document.getElementById("avatar").src = "images/roger.jpeg";
+
+}else if (arrayMail[i] == 'paulMcCartney@rockstar.com') {
+    document.getElementById('rockstarContainer').setAttribute('class','show');
+    document.getElementById('rockstar').innerHTML = '"Paul"';
+    document.getElementById("avatar").src = "images/paul.jpeg";
+
+}else if (arrayMail[i] == 'mickJagger@rockstar.com') {
+    document.getElementById('rockstarContainer').setAttribute('class','show');
+    document.getElementById('rockstar').innerHTML = '"Mick"';
+    document.getElementById("avatar").src = "images/mick.jpeg";
+
+} else {
+document.getElementById('rockstarContainer').setAttribute('class','show');
+document.getElementById('rockstar').innerHTML = 'unknown rockstar';
+document.getElementById("avatar").src = "images/rockstar.png";
 }
